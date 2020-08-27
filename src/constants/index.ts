cd266cea01d3745d2e46e78d269498c97e65c7a2
cuijin <cuijin@svinsight.com>
2020-08-27 12:31:24 +0800

    fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { ChainId, JSBI, Percent, Token, WETH } from '@uniswap/sdk'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0xc7433441a67320a0e48d9c29da797A4920d6dCd0'
 
 // a list of tokens by chain
 type ChainTokenList = {
